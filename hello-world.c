c8d2cf5fee9fae5086767b4575ca657ba3adf3bc
Grace Chu <grc35@pitt.edu>
2019-09-06 11:39:34 -0400

    Add c and python versions of hello world

---
+++ hello-world.c
@@ -0,0 +1,13 @@
+/*
+	hello world in c
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char* s = "Hello world! This is c :)"
+	printf("Hello world! This is c :)\n");
+	printf("%s\n", s);
+}
---
+++ hello-world.py
@@ -0,0 +1,6 @@
+# hello world in python
+
+s = "Hello world!"
+s2 = "This is python :)"
+print("Hello world! This is python :)")
+print(s + " " + s2)
